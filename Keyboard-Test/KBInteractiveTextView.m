88ebeb86ae714fe915b237ece84da773f18c12c0
blaw711 <blaw711@comcast.net>
2014-10-10 00:02:07 -0400

    initial commit, created resizing textview and tableview

---
+++ Keyboard-Test/KBInteractiveTextView.m
@@ -0,0 +1,115 @@
+//
+//  KBInteractiveTextView.m
+//  Keyboard-Test
+//
+//  Created by Bob Law on 10/9/14.
+//  Copyright (c) 2014 Bob Law. All rights reserved.
+//
+
+#import "KBInteractiveTextView.h"
+
+@interface KBInteractiveTextView () <UITextViewDelegate>
+
+@property (nonatomic, strong) UIButton *button;
+
+@property (nonatomic, strong, readwrite) NSLayoutConstraint *heightConstraint;
+
+@end
+
+@implementation KBInteractiveTextView
+
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        self.backgroundColor = [[UIColor lightGrayColor] colorWithAlphaComponent:0.6];
+        
+        self.textView = [[UITextView alloc] init];
+        self.textView.translatesAutoresizingMaskIntoConstraints = NO;
+        self.textView.layer.cornerRadius = 10.0f;
+        self.textView.layer.borderColor = [UIColor lightGrayColor].CGColor;
+        self.textView.layer.borderWidth = 2.0f;
+        self.textView.textContainerInset = UIEdgeInsetsMake(5, 2, 0, 0);
+        self.textView.font = [UIFont fontWithName:@"HelveticaNeue-Thin" size:18.0f];
+        self.textView.delegate = self;
+        [self addSubview:self.textView];
+        
+        self.button = [UIButton buttonWithType:UIButtonTypeCustom];
+        self.button.translatesAutoresizingMaskIntoConstraints = NO;
+        self.button.backgroundColor = [UIColor blueColor];
+        [self addSubview:self.button];
+        
+        self.heightConstraint = [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:40];
+        [self addConstraint:self.heightConstraint];
+        
+        NSDictionary *views = NSDictionaryOfVariableBindings(_textView, _button);
+        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-5-[_button(==30)]-5-[_textView]-5-|" options:0 metrics:0 views:views]];
+        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-5-[_textView]-5-|" options:0 metrics:0 views:views]];
+        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[_button(==30)]-5-|" options:0 metrics:0 views:views]];
+    }
+    
+    return self;
+}
+
+- (void)textViewDidChange:(UITextView *)textView
+{
+    [self refreshHeight];
+}
+
+-(void)resizeTextView:(NSInteger)newSizeH
+{
+//    if ([delegate respondsToSelector:@selector(growingTextView:willChangeHeight:)]) {
+//        [delegate growingTextView:self willChangeHeight:newSizeH];
+//    }
+    self.heightConstraint.constant = newSizeH;
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        [self layoutIfNeeded];
+    }];
+//    CGRect internalTextViewFrame = self.frame;
+//    internalTextViewFrame.size.height = newSizeH; // + padding
+//    self.frame = internalTextViewFrame;
+//    
+//    internalTextViewFrame.origin.y = contentInset.top - contentInset.bottom;
+//    internalTextViewFrame.origin.x = contentInset.left;
+//    
+//    if(!CGRectEqualToRect(internalTextView.frame, internalTextViewFrame)) internalTextView.frame = internalTextViewFrame;
+}
+
+- (void)refreshHeight
+{
+    //size of content, so we can set the frame of self
+    CGFloat minHeight = 40;
+    CGFloat maxHeight = 150;
+    NSInteger newSizeH = [self measureHeight];
+    if (newSizeH < minHeight || !self.textView.text) {
+        newSizeH = minHeight; //not smalles than minHeight
+    }
+    else if (maxHeight && newSizeH > maxHeight) {
+        newSizeH = maxHeight; // not taller than maxHeight
+    }
+    [self resizeTextView:newSizeH];
+    [self.delegate textView:self didChangeToHeight:newSizeH];
+}
+
+
+- (CGFloat)measureHeight
+{
+    if ([self respondsToSelector:@selector(snapshotViewAfterScreenUpdates:)])
+    {
+        return ceilf([self.textView sizeThatFits:self.textView.frame.size].height);
+    }
+    else {
+        return self.textView.contentSize.height + 5;
+    }
+}
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
--- Keyboard-Test/ViewController.m
+++ Keyboard-Test/ViewController.m
@@ -7,8 +7,17 @@
 //
 
 #import "ViewController.h"
+#import "KBInteractiveTextView.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate, KBInteractiveTextViewDelegate>
+
+@property (nonatomic, strong) KBInteractiveTextView *textView;
+@property (nonatomic, strong) NSLayoutConstraint *textViewBottomConstraint;
+@property (nonatomic, strong) NSLayoutConstraint *tableViewHeight;
+
+@property (nonatomic, strong) UITableView *tableView;
+
+@property (nonatomic, strong) NSNumber *keyboardHeight;
 
 @end
 
@@ -16,12 +25,116 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.textView = [[KBInteractiveTextView alloc] init];
+    self.textView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.textView.delegate = self;
+    [self.view addSubview:self.textView];
+    
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+    self.tableView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    [self.view addSubview:self.tableView];
+    
+    self.tableViewHeight = [NSLayoutConstraint constraintWithItem:self.tableView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:CGRectGetHeight([UIScreen mainScreen].bounds) - self.textView.heightConstraint.constant];
+    [self.view addConstraint:self.tableViewHeight];
+
+    
+    self.textViewBottomConstraint = [NSLayoutConstraint constraintWithItem:self.textView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1 constant:0.0];
+    [self.view addConstraint:self.textViewBottomConstraint];
+    
+    
+    //[self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[_textView(==40)]|" options:0 metrics:0 views:NSDictionaryOfVariableBindings(_textView)]];
+    NSDictionary *views = NSDictionaryOfVariableBindings(_textView, _tableView);
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_tableView]|" options:0 metrics:0 views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_tableView]" options:0 metrics:0 views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_textView]|" options:0 metrics:0 views:views]];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyBoardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyBoardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+    [self.view addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(viewWasTapped)]];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)keyBoardWillShow:(NSNotification *)notification
+{
+    NSDictionary* userInfo = notification.userInfo;
+    //keyboardSlideDuration is an instance variable so we can keep it around to use in the "dismiss keyboard" animation.
+    CGFloat keyboardSlideDuration = [[userInfo objectForKey: UIKeyboardAnimationDurationUserInfoKey] floatValue];
+    CGRect keyboardFrame = [[userInfo objectForKey: UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    
+    self.keyboardHeight = @(keyboardFrame.size.height);
+    self.textViewBottomConstraint.constant = -keyboardFrame.size.height;
+    self.tableViewHeight.constant = CGRectGetHeight([UIScreen mainScreen].bounds) - keyboardFrame.size.height - self.textView.heightConstraint.constant;
+    
+    [UIView animateWithDuration:keyboardSlideDuration animations:^{
+        [self.view layoutIfNeeded];
+    } completion:^(BOOL finished){
+        [self.tableView scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:29 inSection:0] atScrollPosition:UITableViewScrollPositionBottom animated:YES];
+    }];
+
+    
+}
+
+- (void)keyBoardWillHide:(NSNotification *)notification
+{
+    NSDictionary* userInfo = notification.userInfo;
+    //keyboardSlideDuration is an instance variable so we can keep it around to use in the "dismiss keyboard" animation.
+   CGFloat keyboardSlideDuration = [[userInfo objectForKey: UIKeyboardAnimationDurationUserInfoKey] floatValue];
+   CGRect keyboardFrame = [[userInfo objectForKey: UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    
+    self.keyboardHeight = @(keyboardFrame.size.height);
+    self.textViewBottomConstraint.constant = 0;
+    self.tableViewHeight.constant = self.tableViewHeight.constant + keyboardFrame.size.height;
+
+    
+    [UIView animateWithDuration:keyboardSlideDuration animations:^{
+        [self.view layoutIfNeeded];
+    }];
+
+}
+
+- (void)viewWasTapped
+{
+    [self.textView.textView resignFirstResponder];
+}
+
+#pragma mark - KBInteractiveTextViewDelegate
+
+- (void)textView:(KBInteractiveTextView *)textView didChangeToHeight:(CGFloat)height
+{
+    self.tableViewHeight.constant = CGRectGetHeight([UIScreen mainScreen].bounds) - self.keyboardHeight.floatValue - height;
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        [self.view layoutIfNeeded];
+    }];
+}
+
+#pragma mark - UITableViewDelegate && UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 30;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"CellIdentifier"];
+    
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"CellIdentifier"];
+    }
+    
+    cell.textLabel.text = @"placeholder";
+    
+    return cell ;
 }
 
 @end
